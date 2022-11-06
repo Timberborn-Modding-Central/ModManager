57103866cfa6b2d995fd0e22d2304101e0a9573c
hytonhan <hannuhytonen@gmail.com>
2022-11-06 14:43:46 +0200

    Added Mod Manager button to mainmenu

---
+++ ModManagerUI/ModManagerUIPlugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModManagerUI
+{
+    [BepInPlugin("com.modmanagerui", "Mod Manager UI", "0.1.0")]
+    public class ModManagerUIPlugin : BaseUnityPlugin
+    {
+        public static ManualLogSource Log;
+
+        private void Awake()
+        {
+            new Harmony("com.modmanagerui").PatchAll();
+        }
+    }
+}
---
+++ ModManagerUI/Patches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Timberborn.MainMenuScene;
+using Timberborn.ModsSystemUI;
+using UnityEngine.UIElements;
+
+namespace ModManagerUI
+{
+    [HarmonyPatch]
+    public class Patches
+    {
+
+        [HarmonyPatch(typeof(MainMenuPanel), "GetPanel")]
+        [HarmonyPostfix]
+        public static void MainMenuPanelPostfix(ref VisualElement __result)
+        {
+            VisualElement root = __result.Query("MainMenuPanel");
+            Button button = new Button() { classList = { "menu-button" } };
+            button.text = "Mod manager";
+            button.clicked += ModsBox.OpenOptionsDelegate;
+            root.Insert(7, button);
+        }
+    }
+}
