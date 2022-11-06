13b00bc1265beab9bd26a4f874a284c908cac814
hytonhan <hannuhytonen@gmail.com>
2022-11-06 13:46:54 +0200

    Moved UI files to UI project

---
+++ ModManagerUI/UIConfigurator.cs
@@ -0,0 +1,19 @@
+﻿using Bindito.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TimberApi.ConfiguratorSystem;
+using Timberborn.ModsSystemUI;
+
+namespace ModManager
+{
+    [Configurator(TimberApi.SceneSystem.SceneEntrypoint.MainMenu)]
+    public class UIConfigurator : IConfigurator
+    {
+        public void Configure(IContainerDefinition containerDefinition)
+        {
+            containerDefinition.Bind<ModsBox>().AsSingleton();
+            containerDefinition.Bind<IModService>().To<ModService>().AsSingleton();
+        }
+    }
+}
