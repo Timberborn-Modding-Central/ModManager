2f7fda0ae8fec780b928572c22f72292c097a883
KYPremco <KYPremco@gmail.com>
2022-11-13 15:45:01 +0100

    Added enabler, finder for maps

---
+++ ModManager/MapSystem/MapEnabler.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using ModManager.EnableSystem;
+using ModManager.EnableSystem.Enablers.MapEnablerSystem;
+using ModManager.ModSystem;
+
+namespace ModManager.MapSystem
+{
+    public class MapEnabler : IModEnabler
+    {
+        public bool Enable(Manifest manifest)
+        {
+            if (manifest is not MapManifest mapManifest)
+            {
+                return false;
+            }
+
+            string enabledFilePath = Path.Combine(Paths.Maps, mapManifest.MapFileName + Names.Extensions.TimberbornMap);
+
+            if (! File.Exists(enabledFilePath + Names.Extensions.Disabled))
+            {
+                return true;
+            }
+
+            File.Move(enabledFilePath + Names.Extensions.Disabled, enabledFilePath);
+
+            return true;
+        }
+
+        public bool Disable(Manifest manifest)
+        {
+            if (manifest is not MapManifest mapManifest)
+            {
+                return false;
+            }
+
+            string mapFilePath = Path.Combine(Paths.Maps, mapManifest.MapFileName + Names.Extensions.TimberbornMap);
+
+            if (! File.Exists(mapFilePath))
+            {
+                return true;
+            }
+
+            File.Move(mapFilePath, mapFilePath + Names.Extensions.Disabled);
+
+            return true;
+        }
+    }
+}
---
+++ ModManager/MapSystem/MapManifestFinder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using ModManager.EnableSystem.Enablers.MapEnablerSystem;
+using ModManager.ManifestFinderSystem;
+using ModManager.ModSystem;
+using ModManager.PersistenceSystem;
+
+namespace ModManager.MapSystem
+{
+    public class MapManifestFinder : IManifestLocationFinder
+    {
+        private readonly PersistenceService _persistenceService;
+
+
+        public MapManifestFinder()
+        {
+            _persistenceService = PersistenceService.Instance;
+        }
+
+        public IEnumerable<Manifest> Find()
+        {
+            string manifestPath = Path.Combine(Paths.Maps, Manifest.FileName);
+
+            if (! File.Exists(manifestPath))
+            {
+                return new List<Manifest>();
+            }
+
+            return _persistenceService.LoadObject<List<MapManifest>>(manifestPath);
+        }
+    }
+}
