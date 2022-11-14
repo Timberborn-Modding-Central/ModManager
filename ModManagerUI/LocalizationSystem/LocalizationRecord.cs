483ad333143450e97d138a088fd4273325410946
hytonhan <hannuhytonen@gmail.com>
2022-11-14 21:12:42 +0200

    Added loc service

---
+++ ModManagerUI/LocalizationSystem/LocalizationRecord.cs
@@ -0,0 +1,21 @@
+﻿using LINQtoCSV;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModManagerUI.LocalizationSystem
+{
+    internal class LocalizationRecord
+    {
+        [CsvColumn(Name = "ID")]
+        public string Id { get; set; } = null!;
+
+        [CsvColumn(Name = "Text")]
+        public string Text { get; set; } = null!;
+
+        [CsvColumn(Name = "Comment")]
+        public string Comment { get; set; } = null!;
+
+        public bool IsWip { get; set; }
+    }
+}
