6e96b00d0a92489651906af480845a073d6bbbd2
hytonhan <hannuhytonen@gmail.com>
2022-11-21 19:34:04 +0200

    Revert "mark file with .remove if it can't be deleted"
    This reverts commit f1d095e923df37f263c7cf3342a9744dbcd4cb46.

--- ModManagerUI/ModManagerUIPlugin.cs
+++ ModManagerUI/ModManagerUIPlugin.cs
@@ -18,15 +18,7 @@ namespace ModManagerUI
         public void Awake()
         {
             Log = Logger;
-            ModManagerStartup.Run("7f52d134de5cde63fdcf163478e688e3", (options) =>
-            {
-                options.GameId = 3659;
-                options.GamePath = @"D:\Ohjelmat\Steam\steamapps\common\Timberborn";
-                options.IsGameRunning = true;
-                options.ModInstallationPath = @"D:\Ohjelmat\Steam\steamapps\common\Timberborn\mods";
-                options.ModIoGameUrl = "https://mod.io/g/timberborn";
-                options.ModManagerPath = @"D:\Ohjelmat\Steam\steamapps\common\Timberborn\BepInEx\plugins\ModManager";
-            });
+            // Add ModManagerStartup.Run() here
 
             var harmony = new Harmony("com.modmanagerui");
             harmony.PatchAll();
