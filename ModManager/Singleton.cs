8b10e638f1f1795665d9c8a172f4a38713d55099
hytonhan <hannuhytonen@gmail.com>
2023-01-07 10:54:03 +0200

    Made Singleton a little safer

--- ModManager/Singleton.cs
+++ ModManager/Singleton.cs
@@ -1,10 +1,13 @@
-﻿namespace ModManager
+﻿using System;
+
+namespace ModManager
 {
     public abstract class Singleton<T> where T : class, new()
     {
         // ReSharper disable once InconsistentNaming
-        private static readonly T? _instance;
+        private static Lazy<T> _instance = new Lazy<T>(() => new T());
 
-        public static T Instance = _instance ??= new T();
+        public static T Instance => _instance.Value;
+
     }
 }
