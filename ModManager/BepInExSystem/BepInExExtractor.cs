b18e8249fedadbef5469e454e84a2965b773f31c
hytonhan <hannuhytonen@gmail.com>
2023-10-27 21:23:19 +0300

    Added some code so bep fodlers in plugins folder are handled

--- ModManager/BepInExSystem/BepInExExtractor.cs
+++ ModManager/BepInExSystem/BepInExExtractor.cs
@@ -1,12 +1,18 @@
 ﻿using Modio.Models;
 using ModManager.ExtractorSystem;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
+using System.Linq;
 
 namespace ModManager.BepInExSystem
 {
     public class BepInExExtractor : IAddonExtractor
     {
         private const string _bepInExPackName = "BepInExPack";
+
+        private List<string> _foldersToIgnore = new() { "" };
 
         // TODO: Add actual extract logic from bepinex. 
         //       Atm bepinex must be installed already for mod manager to work
@@ -19,6 +25,11 @@ namespace ModManager.BepInExSystem
                 return false;
             }
 
+            ZipFile.ExtractToDirectory(addonZipLocation, Paths.GameRoot, true);
+
+            string modFolderName = $"{modInfo.NameId}_{modInfo.Id}_{modInfo.Modfile.Version}";
+            ClearOldModFiles(modInfo, modFolderName);
+
             extractLocation = Path.Combine(Paths.GameRoot, "BepInEx", "plugins", $"{modInfo.NameId}_{modInfo.Id}_{modInfo.Modfile.Version}");
             if (!Directory.Exists(extractLocation))
             {
@@ -28,6 +39,89 @@ namespace ModManager.BepInExSystem
             System.IO.File.Delete(addonZipLocation);
 
             return true;
+        }
+        private void ClearOldModFiles(Mod modInfo, string modFolderName)
+        {
+            if (TryGetExistingModFolder(modInfo, out string dirs))
+            {
+                var dirInfo = new DirectoryInfo(dirs);
+                if (dirInfo.Name.Equals(modFolderName))
+                {
+                    return;
+                }
+                dirInfo.MoveTo(Path.Combine(Paths.Mods, modFolderName));
+                DeleteModFiles(modFolderName);
+            }
+        }
+        private bool TryGetExistingModFolder(Mod modInfo, out string dirs)
+        {
+            dirs = null;
+            try
+            {
+                dirs = Directory.GetDirectories(Paths.Mods, $"{modInfo.NameId}_{modInfo.Id}*").SingleOrDefault();
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new AddonExtractorException($"Found multiple folders for \"{modInfo.Name}\"");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            if (dirs != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        private void DeleteModFiles(string modFolderName)
+        {
+            var modDirInfo = new DirectoryInfo(Path.Combine(Paths.Mods, modFolderName));
+            var modSubFolders = modDirInfo.GetDirectories("*", SearchOption.AllDirectories)
+                                          .Where(folder => !_foldersToIgnore.Contains(folder.FullName.Split(Path.DirectorySeparatorChar).Last()));
+            foreach (DirectoryInfo subDirectory in modSubFolders.Reverse())
+            {
+                DeleteFilesFromFolder(subDirectory);
+                TryDeleteFolder(subDirectory);
+            }
+
+            DeleteFilesFromFolder(modDirInfo);
+            TryDeleteFolder(modDirInfo);
+        }
+
+        private void DeleteFilesFromFolder(DirectoryInfo dir)
+        {
+            foreach (FileInfo file in dir.GetFiles().Where(file => !file.Name.EndsWith(Names.Extensions.Remove)))
+            {
+                try
+                {
+                    file.Delete();
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    file.MoveTo($"{file.FullName}{Names.Extensions.Remove}");
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        private void TryDeleteFolder(DirectoryInfo dir)
+        {
+            try
+            {
+                if (dir.EnumerateDirectories().Any() == false && dir.EnumerateFiles().Any() == false)
+                {
+                    dir.Delete();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
